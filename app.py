235153dcdcd476ae58383bc297c2ccd2e7644e46
ncku_desktop <dushiun@gmail.com>
2021-03-22 14:33:33 +0800

    "update 0322_student_version"

--- app.py
+++ app.py
@@ -28,23 +28,10 @@ def callback():
             user_id = f.read()
         f.close()
         sensor_signal = request.args.get(key='SENSOR')
-        # predict_signal = request.args.get(key="RESULT")##此處程式碼為辨識影像結果
         if (sensor_signal=="ON"):
-            # line_bot_api.broadcast(TextSendMessage(text='SENSOR IS ON SIGNAL'))#廣播通知全部人
-            # line_bot_api.broadcast(TextSendMessage(text='This is a broadcast message'))#廣播通知全部人
+            line_bot_api.broadcast(TextSendMessage(text='SENSOR IS ON SIGNAL'))#廣播通知全部人
+            line_bot_api.broadcast(TextSendMessage(text='This is a broadcast message'))#廣播通知全部人
             line_bot_api.push_message(user_id, TextSendMessage(text='Message from Desktop send to specific id'))#向特定人傳送訊息
-################################# 此處程式碼為辨識影像結果
-        # if (predict_signal == "0 khduh"):
-        #     line_bot_api.push_message(user_id, TextSendMessage(text='Welcome home'))
-        # elif (predict_signal == "1 mask"):
-        #     line_bot_api.push_message(user_id, TextSendMessage(text='Warning'))
-        #     line_bot_api.push_message(user_id,
-        #                               ImageSendMessage(original_content_url="https://80e54e6189c5.ngrok.io/photo_page",
-        #                                                preview_image_url="https://80e54e6189c5.ngrok.io/photo_page"))
-        # else:
-        #     line_bot_api.push_message(user_id, TextSendMessage(text='No predicting'))
-        #     pass
-#################################此處程式碼為辨識影像結果
 
         return "OK"
     elif request.method == 'POST':
@@ -64,7 +51,7 @@ def handle_message(event):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text))
-    if event.message.text == "setreply":
+    if event.message.text == "setreply":#設定使用者ID
         var_id = (event.source.user_id)
         with open('id.txt', 'w') as f:
             f.write(str(var_id))
